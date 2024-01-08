d66d1415726bd640e8c1fb33eed538bf003c8980
Antonio Ramos <117685319+ramoa1234@users.noreply.github.com>
2024-01-08 12:47:56 -0500

    Update server.js

--- server.js
+++ server.js
@@ -3,9 +3,29 @@ const app = express();
 const port = 3000;
 const mysql = require('mysql');
 
-const db = mysql.createConnection({
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'antonio',
+    passwoord: 'Pp12/13/2003',
+    database: 'test',
+    });
+    
+    connection.connect((err) => {
+        if(err) {
+            console.log('Error connecting to Db' + err.stack);
+            return;
+        }
+        console.log('Connection established');
 
-});
+        connection.query('SELECT * FROM users', (err, rows) => {
+            if(err) throw err;
+        
+            console.log('Data received from Db:');
+            console.log(rows);
+        });
+    
+    });
+
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
