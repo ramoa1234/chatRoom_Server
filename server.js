9d4ccd99e2c0c5b4dec00e3b5e86e3d6d8e17bc4
Antonio Ramos <117685319+ramoa1234@users.noreply.github.com>
2024-01-16 02:19:51 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,87 +1,55 @@
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+const path = require('path');
+const port = 3000;
 
+const users = [];
 
-    const express = require('express');
-    const app = express();
-    const port = 3000;
-    const mysql = require('mysql2');
-    const bodyParser = require('body-parser');
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: false }));
 
-    app.use(express.urlencoded({ extended: true }));
+app.get('/', (req, res) => {
+    res.render('index');
+});
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+app.get('/register', (req, res) => {
+    res.render('register');
+});
 
-    const connection = mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: 'Pp12/13/2003',
-        database: 'data',
-        });
-        
-        connection.connect((err) => {
-            if (err) {
-                console.error('An error occurred while connecting to the DB');
-                throw err;
-            }
-            console.log('Connection established');
-        });
-
-
-
-
-
-
-
-
-    app.set('view engine', 'ejs');
-    app.use(express.static('public'));
-
-    app.get('/', (req, res) => {
-        res.render('index.ejs');
-    });
-
-
-    app.get('/login', (req, res) => {
-        res.render('login.ejs');
-    });
-    app.post('/login', (req, res) => {
-        const email = req.body.email;
-        const password = req.body.password;
-        
-        checkUser(email, password, (err, userExist) => {
-            if (err) {
-                res.status(500).send('An error occurred while checking the user');
-            }
-            if (userExist) {
-                console.log('User exists');
-            } else {
-                console.log('no user');
-            }
-        });
-        
-    });
-
-
-    app.get('/register', (req, res) => {
-        res.render('register.ejs');
-    });
-
-    app.listen(port, () => {
-        console.log(`Server listening at http://localhost:${port}`);
-    });
+app.post('/register', async  (req , res) => {
+    try {
+        console.log('before push')
+        users.push({
+            id: Date.now().toString(),
+            username: req.body.username,
+            email: req.body.email,
+            password: req.body.password
+        })
+        console.log('after push')
+        res.redirect('/login')
+    } catch {
+        console.log('catch')
+        res.redirect('/register')
+    }
+    console.log(users);
+})
+app.post('/login' , (req, res) => {
     
 
+})
 
+server.listen(port, () => {
+    console.log('server running');
+})
+io.on('connection', (socket) => {
+    console.log("User Connected:" + socket.id);
 
+    socket.on("message", (data) => {
+        socket.broadcast.emit("message", data);
+    });
 
-    function  checkUser(email, password, callback) {
-        console.log(`Checking user with email: ${email} and password: ${password}`);
-        const query = 'SELECT * FROM users WHERE email = ? AND password = ?';
-        
-        connection.query(query, [email, password], (err, results) => {
-            if (err) {
-                return callback(err);
-            }
-            const userExist = results.length > 0;
-            return callback(null, userExist);
-        
-        });
-    }
-
+})
